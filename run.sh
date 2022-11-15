02bb5b55169c2c76c6ff1a46a56d5f1359a81baf
Vicente Pavez <vicentepavez04@gmail.com>
2022-11-15 03:42:19 -0300

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
-weight=/path/to/original/stable-diffusion/sd-v1-4-full-ema.ckpt
-text="a photo of a thermal face"
-python scripts/stable_txt2img.py --ddim_eta 0.0 --n_samples 8 --n_iter 1 --scale 10.0 --ddim_steps 50  --ckpt $weight --prompt "${text}" 
+weight=backup/thermalface.ckpt
+text="a photo of Barack Obama, grey, thermal, thermalface"
+
+start=$$
+END=$(( 20+$start ))
+echo "start seed $start"
+echo "end seed $END"
+for i in $(seq $start $END); do
+python scripts/stable_txt2img.py --seed $i --ddim_eta 0.0 --n_samples 3 --n_iter 1 --scale 10.0 --ddim_steps 50  --ckpt $weight --prompt "$>
+done
